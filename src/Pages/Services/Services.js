0144c86a8db40514648964e0e01c23954401f2d9
sultanmahmud07 <asadkhan01862@gmail.com>
2022-11-09 20:59:05 +0600

    some files styles changed

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -10,16 +10,16 @@ const Services = () => {
   .then(data => setServices(data))
   }, [])
   return (
-    <div className='common-w py-6'>
+    <div className='common-w py-6 lg:pt-20'>
       <div>
-      <h1 className='text-6xl text-primary text-center'>Out Services !!</h1>
-      <p className='lg:w-2/4 text-center m-auto py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quasi eos id cum illo tenetur magnam deleniti. Tenetur, necessitatibus reprehenderit.</p>
+      <h1 className='text-5xl common-c text-center font-semibold uppercase'>My Services</h1>
+      <p className='lg:w-2/4 text-center m-auto py-4 text-xl lg:pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quasi eos id cum illo tenetur magnam deleniti. Tenetur, necessitatibus reprehenderit.</p>
 
       </div>
       {/* service containers */}
 
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
       {
         services.map(service => <ServiceCard
           key={service._id}
@@ -28,7 +28,8 @@ const Services = () => {
       }
       </div>
       <div className='text-center py-7'>
-        <button className="btn btn-primary"><Link to='/services'>See All</Link></button>
+      <Link to='/services'><button className="btn btn-primary">Show all</button></Link>
+        
       </div>
     </div>
   );
--- src/Share/Header/Header.js
+++ src/Share/Header/Header.js
@@ -2,11 +2,14 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../../CommonStyles/Common.css';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 
 
 const Header = () => {
   const {logOut, user} =useContext(AuthContext);
+  console.log(user?.displayName);
+  console.log(user?.photoURL);
 
 
   const handleLogOut = () => {
@@ -56,21 +59,21 @@ const Header = () => {
   
 </>
   return (
-    <div className='border-b-2 mb-3'>
-      <div className="navbar bg-base-100 common-w">
+    <div className='border-b-2 h-color mb-3'>
+      <div className="navbar  common-w">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow h-color rounded-box w-52">
           {
             menuItems
           }
 
             </ul>
           </div>
-          <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+          <Link to='/' className="btn btn-ghost normal-case text-2xl"><span className='q'>Q</span><span className='i'>i</span>-Photo</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
@@ -80,8 +83,22 @@ const Header = () => {
           </ul>
         </div>
         <div className="navbar-end">
-        
-          <a className="btn">Get started</a>
+          {
+            user?.uid ?
+             <>
+                <span className='user-name mr-1'>{user?.displayName}</span>
+          <img className='w-12 rounded-full' src={user?.photoURL} alt="" />
+             </>
+              :
+               <>
+               <span className='user-name mr-1'>User Name</span>
+               <FaUserCircle className='text-5xl'></FaUserCircle>
+               </>
+          }
+          
+         
+          
+         
         </div>
       </div>
     </div>
