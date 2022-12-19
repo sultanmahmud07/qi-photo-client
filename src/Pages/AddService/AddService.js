6a9c14860fe7461b58f3db1885b3744229afb043
sultanmahmud07 <asadkhan01862@gmail.com>
2022-12-19 17:10:18 +0600

    serivice added complate

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,7 +3,22 @@ import React from 'react';
 const AddService = () => {
   return (
     <div className='common-w'>
-      <p className='text-xl md:text-6xl text-center text-blue-300 font-semibold md:py-40'>No Service were added</p>
+      <div>
+        <form className='max-w-2xl mx-auto my-11 py-10 p-6 shadow-xl'>
+          <h1 className='text-center text-4xl py-10'>Add your product</h1>
+          <span className='p-3 text-xl mt-4'>Service Name</span>
+          <input type="text" name='title' placeholder="enter service title" className="input input-bordered input-primary w-full" />
+          <span className='p-3 text-xl'>Service Description</span>
+          <input type="text" name='Desc' placeholder="enter service description" className="input input-bordered input-primary w-full" />
+          <span className='p-3 text-xl'>Price</span>
+          <input type="text" name='price' placeholder="enter service price" className="input input-bordered input-primary w-full" />
+          <span className='p-2 text-xl mt-4'>Rating</span>
+          <input type="text" name='rating' placeholder="Type here" className="input input-bordered input-primary w-full" />
+          <span className='p-2 text-xl mt-4'>Title</span>
+          <input type="file" className="file-input file-input-bordered file-input-primary w-full " />
+          <input type="submit" value='Add Product' className='my-5 w-full bg-primary text-white font-bold text-xl p-3' />
+        </form>
+      </div>
     </div>
   );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Blog.css';
+
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const Blog = () => {
@@ -9,50 +10,50 @@ const Blog = () => {
         <title>Blog-Qi-Photo</title>
       </Helmet>
       <div className='common-w'>
-      <div className=" text-center d-block">
-        <h1 className='blog-title topic-header text-5xl py-4 font-semibold my-5'>These are Blogs!!!</h1>
-      </div>
-      <div className="container mt-2">
-        <div className="single-question">
-          <div className="title-wrapper">
-            <div >
-              <h2 className="q-title">1. Difference between SQL and NoSQL</h2>
-            </div>
-          </div>
-          <p className='q-p'><span className='ans'>Ans:</span> SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
+        <div className=" text-center d-block">
+          <h1 className='blog-title topic-header text-5xl py-4 font-semibold my-5'>These are Blogs!!!</h1>
         </div>
-        <div className="single-question">
-          <div className="title-wrapper">
-            <div>
-              <h2 className="q-title">2. What Is JWT? How Does It Work? </h2>
+        <div className="container mt-2">
+          <div className="single-question">
+            <div className="title-wrapper">
+              <div >
+                <h2 className="q-title">1. Difference between SQL and NoSQL</h2>
+              </div>
             </div>
+            <p className='q-p'><span className='ans'>Ans:</span> SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
           </div>
-          <p className='q-p'><span className='ans'>Ans:</span> What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
+          <div className="single-question">
+            <div className="title-wrapper">
+              <div>
+                <h2 className="q-title">2. What Is JWT? How Does It Work? </h2>
+              </div>
+            </div>
+            <p className='q-p'><span className='ans'>Ans:</span> What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
+          </div>
+
+          <div className="single-question">
+            <div className="title-wrapper">
+              <div>
+                <h2 className="q-title">3. What is the difference between javascript and Node JS</h2>
+              </div>
+            </div>
+            <p className='q-p'><span className='ans'>Ans:</span> JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language</p>
+
+          </div>
+          <div className="single-question">
+            <div className="title-wrapper">
+              <div>
+                <h2 className="q-title">4. how does Node JS handle multiple requests at the same time?</h2>
+              </div>
+            </div>
+
+            <p className='q-p'><span className='ans'>Ans:</span>  NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
+
+          </div>
         </div>
 
-        <div className="single-question">
-          <div className="title-wrapper">
-            <div>
-              <h2 className="q-title">3. What is the difference between javascript and Node JS</h2>
-            </div>
-          </div>
-          <p className='q-p'><span className='ans'>Ans:</span> JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language</p>
-          
-        </div>
-        <div className="single-question">
-          <div className="title-wrapper">
-            <div>
-              <h2 className="q-title">4. how does Node JS handle multiple requests at the same time?</h2>
-            </div>
-          </div>
-        
-          <p className='q-p'><span className='ans'>Ans:</span>  NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
-          
-        </div>
-      </div>
-      
 
-    </div>
+      </div>
     </HelmetProvider>
   );
 };
